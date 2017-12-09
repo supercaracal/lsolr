a5d5663dbc0f65cb0b8615ac295c2a7a58d5d822
Taishi Kasuga <proxy0721@gmail.com>
2017-12-10 00:27:28 +0900

    Enhance error handling

--- lib/lsolr.rb
+++ lib/lsolr.rb
@@ -5,47 +5,53 @@ require 'date'
 # A query builder of Apache Solr standard Lucene type query for Ruby.
 #
 # @example How to use. Part 1:
-#   LSolr.build(term1: 'hoge', term2: true).to_s
-#   #=> 'term1:hoge AND term2:true'
+#   LSolr.build(field1: 'hoge', field2: true).to_s
+#   #=> 'field1:hoge AND field2:true'
 #
 # @example How to use. Part 2:
 #    params = {
-#      term01: 'hoge',
-#      term02: :fuga,
-#      term03: 14,
-#      term04: 7.3,
-#      term05: true,
-#      term06: false,
-#      term07: Date.new(7000, 7, 1),
-#      term08: DateTime.new(6000, 5, 31, 6, 31, 43),
-#      term09: Time.new(5000, 6, 30, 12, 59, 3),
-#      term10: LSolr.new(:term10).fuzzy_match('foo'),
-#      term11: [1, 2, 3],
-#      term12: 1..10,
-#      term13: 20...40,
-#      term14: Date.new(3000, 1, 1)..Date.new(4000, 12, 31),
-#      term15: (3.0..4.0).step(0.1)
+#      field01: 'hoge',
+#      field02: :fuga,
+#      field03: 14,
+#      field04: 7.3,
+#      field05: true,
+#      field06: false,
+#      field07: Date.new(7000, 7, 1),
+#      field08: DateTime.new(6000, 5, 31, 6, 31, 43),
+#      field09: Time.new(5000, 6, 30, 12, 59, 3),
+#      field10: LSolr.new(:field10).fuzzy_match('foo'),
+#      field11: [1, 2, 3],
+#      field12: 1..10,
+#      field13: 20...40,
+#      field14: Date.new(3000, 1, 1)..Date.new(4000, 12, 31),
+#      field15: (3.0..4.0).step(0.1)
 #    }
 #
 #    LSolr.build(params).to_s
-#    #=> 'term01:hoge AND term02:fuga AND term03:14 AND term04:7.3 AND term05:true
-#    #    AND term06:false AND term07:"7000-07-01T00:00:00Z" AND term08:"6000-05-31T06:31:43Z"
-#    #    AND term09:"5000-06-30T12:59:03Z" AND term10:foo~2.0 AND (term11:1 OR term11:2 OR term11:3)
-#    #    AND term12:[1 TO 10] AND term13:[20 TO 40} AND term14:[3000-01-01T00:00:00Z TO 4000-12-31T00:00:00Z]
-#    #    AND term15:[3.0 TO 4.0]'
+#    #=> 'field01:hoge AND field02:fuga AND field03:14 AND field04:7.3 AND field05:true
+#    #    AND field06:false AND field07:"7000-07-01T00:00:00Z" AND field08:"6000-05-31T06:31:43Z"
+#    #    AND field09:"5000-06-30T12:59:03Z" AND field10:foo~2.0 AND (field11:1 OR field11:2 OR field11:3)
+#    #    AND field12:[1 TO 10] AND field13:[20 TO 40} AND field14:[3000-01-01T00:00:00Z TO 4000-12-31T00:00:00Z]
+#    #    AND field15:[3.0 TO 4.0]'
 #
 # @example How to use. Part 3:
-#   monoclinic = LSolr.new(:crystal_system).match(:monoclinic)
-#   cubic = LSolr.new(:crystal_system).match(:cubic)
-#   soft = LSolr.new(:mohs_scale).greater_than_or_equal_to('*').less_than(5.0)
-#   hard = LSolr.new(:mohs_scale).greater_than_or_equal_to(5.0).less_than_or_equal_to(10.0)
+#    bool1 = LSolr.new(:bool_field).match(true)
+#    bool2 = LSolr.new(:bool_field).match(false)
+#    date1 = LSolr.new(:date_field1).greater_than_or_equal_to('*').less_than_or_equal_to(Time.new(2000, 6, 30, 23, 59, 59))
+#    date2 = LSolr.new(:date_field2).greater_than(Time.new(2000, 7, 1, 0, 0, 0)).less_than(Time.new(2001, 1, 1, 0, 0, 0))
 #
-#   phosphophyllite = monoclinic.and(soft).wrap
-#   diamond = cubic.and(hard).wrap
+#    left = bool1.and(date1).and(date2).wrap
+#    right = bool2.and(date1.or(date2).wrap).wrap
 #
-#   phosphophyllite.or(diamond).to_s
-#   #=> '(crystal_system:monoclinic AND mohs_scale:[* TO 5.0}) OR (crystal_system:cubic AND mohs_scale:[5.0 TO 10.0])'
+#    left.or(right).to_s
+#    #=> '(bool_field:true AND date_field1:[* TO 2000-06-30T23:59:59Z] AND date_field2:{2000-07-01T00:00:00Z TO 2001-01-01T00:00:00Z})
+#    #    OR (bool_field:false AND (date_field1:[* TO 2000-06-30T23:59:59Z] OR date_field2:{2000-07-01T00:00:00Z TO 2001-01-01T00:00:00Z}))'
 class LSolr
+  ArgumentError = Class.new(::ArgumentError)
+  RangeError = Class.new(::RangeError)
+  TypeError = Class.new(::TypeError)
+  IncompleteQueryError = Class.new(StandardError)
+
   NOT = 'NOT'
   AND = 'AND'
   OR = 'OR'
@@ -95,7 +101,7 @@ class LSolr
       when Array then build_array_query(field, value)
       when Range then build_range_query(field, value)
       when Enumerator then build_enumerator_query(field, value)
-      else raise ArgumentError, "Could not build solr query. field: #{field}, value: #{value.inspect}"
+      else raise TypeError, "Could not build solr query. field: #{field}, value: #{value.inspect}"
       end
     end
 
@@ -140,7 +146,7 @@ class LSolr
   # @return [String] a stringified query
   def to_s
     @value = "#{@range_first} #{TO} #{@range_last}" if range_search?
-    raise 'Please specify a search condition.' if blank?
+    raise IncompleteQueryError, 'Please specify a search value.' if blank?
 
     expr = "#{left_parentheses.join}#{@not}#{@field}:#{@value}#{right_parentheses.join}"
     expr = "#{prev} #{operator} #{expr}" if !prev.nil? && prev.present?
@@ -385,7 +391,7 @@ class LSolr
     msec_str = case date
                when Date then date.strftime(FORMAT_MILLISECOND_FOR_DATE_TYPE).gsub(date.strftime(FORMAT_SECOND), '')
                when Time then date.strftime(FORMAT_MILLISECOND_FOR_TIME_TYPE)
-               else raise ArgumentError, "Unknown type #{date.inspect}"
+               else raise TypeError, "Could not format dates or times. Given value: #{date.inspect}"
                end
 
     return date.strftime(FORMAT_DATE_TIME) if msec_str == '000'
--- test/lsolr_test.rb
+++ test/lsolr_test.rb
@@ -1,56 +1,57 @@
 # frozen_string_literal: true
 
+require 'time'
 require 'test/unit'
 require 'lsolr'
 
 class LSolrTest < Test::Unit::TestCase
   def test_build
     params = {
-      term01: 'hoge',
-      term02: :fuga,
-      term03: 14,
-      term04: 7.3,
-      term05: true,
-      term06: false,
-      term07: Date.new(7000, 7, 1),
-      term08: DateTime.new(6000, 5, 31, 6, 31, 43), # rubocop:disable Style/DateTime
-      term09: Time.new(5000, 6, 30, 12, 59, 3),
-      term10: LSolr.new(:term10).fuzzy_match('foo'),
-      term11: [1, 2, 3],
-      term12: 1..10,
-      term13: 20...40,
-      term14: Date.new(3000, 1, 1)..Date.new(4000, 12, 31),
-      term15: (3.0..4.0).step(0.1)
+      field01: 'hoge',
+      field02: :fuga,
+      field03: 14,
+      field04: 7.3,
+      field05: true,
+      field06: false,
+      field07: Date.new(7000, 7, 1),
+      field08: DateTime.new(6000, 5, 31, 6, 31, 43), # rubocop:disable Style/DateTime
+      field09: Time.new(5000, 6, 30, 12, 59, 3),
+      field10: LSolr.new(:field10).fuzzy_match('foo'),
+      field11: [1, 2, 3],
+      field12: 1..10,
+      field13: 20...40,
+      field14: Date.new(3000, 1, 1)..Date.new(4000, 12, 31),
+      field15: (3.0..4.0).step(0.1)
     }
 
-    expected = 'term01:hoge AND term02:fuga AND term03:14 AND term04:7.3 AND term05:true AND term06:false'\
-      ' AND term07:"7000-07-01T00:00:00Z" AND term08:"6000-05-31T06:31:43Z" AND term09:"5000-06-30T12:59:03Z"'\
-      ' AND term10:foo~2.0 AND (term11:1 OR term11:2 OR term11:3) AND term12:[1 TO 10] AND term13:[20 TO 40}'\
-      ' AND term14:[3000-01-01T00:00:00Z TO 4000-12-31T00:00:00Z] AND term15:[3.0 TO 4.0]'
+    expected = 'field01:hoge AND field02:fuga AND field03:14 AND field04:7.3 AND field05:true AND field06:false'\
+      ' AND field07:"7000-07-01T00:00:00Z" AND field08:"6000-05-31T06:31:43Z" AND field09:"5000-06-30T12:59:03Z"'\
+      ' AND field10:foo~2.0 AND (field11:1 OR field11:2 OR field11:3) AND field12:[1 TO 10] AND field13:[20 TO 40}'\
+      ' AND field14:[3000-01-01T00:00:00Z TO 4000-12-31T00:00:00Z] AND field15:[3.0 TO 4.0]'
 
     assert_equal expected, LSolr.build(params).to_s
-    assert_equal 'term1:hoge AND term2:true', LSolr.build(term1: 'hoge', term2: true).to_s
-    assert_raise(ArgumentError, 'Could not build solr query. field: term, value: nil') { LSolr.build(term: nil) }
+    assert_equal 'field1:hoge AND field2:true', LSolr.build(field1: 'hoge', field2: true).to_s
+    assert_raise(LSolr::TypeError, 'Could not build solr query. field: f, value: nil') { LSolr.build(f: nil) }
   end
 
   def test_initialize
-    assert_raise(ArgumentError, 'Please specify a field name.') { LSolr.new('') }
+    assert_raise(LSolr::ArgumentError, 'Please specify a field name.') { LSolr.new('') }
   end
 
   def test_to_s
-    assert_raise(RuntimeError, 'Please specify a search condition.') { LSolr.new(:field).to_s }
+    assert_raise(LSolr::IncompleteQueryError, 'Please specify a search value.') { LSolr.new(:field).to_s }
 
-    monoclinic = LSolr.new(:crystal_system).match(:monoclinic)
-    cubic = LSolr.new(:crystal_system).match(:cubic)
-    soft = LSolr.new(:mohs_scale).greater_than_or_equal_to('*').less_than(5.0)
-    hard = LSolr.new(:mohs_scale).greater_than_or_equal_to(5.0).less_than_or_equal_to(10.0)
+    bool1 = LSolr.new(:bool_field).match(true)
+    bool2 = LSolr.new(:bool_field).match(false)
+    date1 = LSolr.new(:date_field1).greater_than_or_equal_to('*').less_than_or_equal_to(Time.new(2000, 6, 30, 23, 59, 59))
+    date2 = LSolr.new(:date_field2).greater_than(Time.new(2000, 7, 1, 0, 0, 0)).less_than(Time.new(2001, 1, 1, 0, 0, 0))
 
-    phosphophyllite = monoclinic.and(soft).wrap
-    diamond = cubic.and(hard).wrap
+    left = bool1.and(date1).and(date2).wrap
+    right = bool2.and(date1.or(date2).wrap).wrap
 
-    expected = '(crystal_system:monoclinic AND mohs_scale:[* TO 5.0})'\
-      ' OR (crystal_system:cubic AND mohs_scale:[5.0 TO 10.0])'
-    assert_equal expected, phosphophyllite.or(diamond).to_s
+    expected = '(bool_field:true AND date_field1:[* TO 2000-06-30T23:59:59Z] AND date_field2:{2000-07-01T00:00:00Z TO 2001-01-01T00:00:00Z})'\
+      ' OR (bool_field:false AND (date_field1:[* TO 2000-06-30T23:59:59Z] OR date_field2:{2000-07-01T00:00:00Z TO 2001-01-01T00:00:00Z}))'
+    assert_equal expected, left.or(right).to_s
   end
 
   def test_blank?
@@ -83,8 +84,8 @@ class LSolrTest < Test::Unit::TestCase
     assert_equal 'field:word^1.5', LSolr.new(:field).match('word').boost(1.5).to_s
     assert_equal 'field:word^1.5', LSolr.new(:field).boost(1.5).match('word').to_s
     assert_equal 'field:word^0.1', LSolr.new(:field).boost(0.1).match('word').to_s
-    assert_raise(ArgumentError, 'The boost factor numver must be positive. 0.0 given.') { LSolr.new(:field).match('word').boost(0.0) }
-    assert_raise(ArgumentError, 'The boost factor numver must be positive. -0.1 given.') { LSolr.new(:field).match('word').boost(-0.1) }
+    assert_raise(LSolr::ArgumentError, 'The boost factor numver must be positive. 0.0 given.') { LSolr.new(:field).match('word').boost(0.0) }
+    assert_raise(LSolr::ArgumentError, 'The boost factor numver must be positive. -0.1 given.') { LSolr.new(:field).match('word').boost(-0.1) }
   end
 
   def test_match
@@ -99,6 +100,7 @@ class LSolrTest < Test::Unit::TestCase
     assert_equal 'field:"2000-01-01T12:00:00Z"', LSolr.new(:field).date_time_match('2000-01-01T12:00:00Z').to_s
     assert_equal 'field:"2000-01-01T12:00:00Z"', LSolr.new(:field).date_time_match(Time.new(2000, 1, 1, 12, 0, 0)).to_s
     assert_equal 'field:"2000-01-01T00:00:00Z"', LSolr.new(:field).date_time_match(Date.new(2000, 1, 1)).to_s
+    assert_equal 'field:"2000-04-05T06:07:08.256Z"', LSolr.new(:field).date_time_match(Time.parse('2000-04-05 06:07:08.256')).to_s
   end
 
   def test_prefix_match
@@ -116,8 +118,8 @@ class LSolrTest < Test::Unit::TestCase
     assert_equal 'field:word~0.0', LSolr.new(:field).fuzzy_match('word', distance: 0.0).to_s
     assert_equal 'field:word~2.0', LSolr.new(:field).fuzzy_match('word', distance: 2.0).to_s
     assert_equal 'field:word~2.0', LSolr.new(:field).fuzzy_match('word').to_s
-    assert_raise(RangeError, 'Out of 0.0..1.0. -0.1 given.') { LSolr.new(:field).fuzzy_match('word', distance: -0.1).to_s }
-    assert_raise(RangeError, 'Out of 0.0..1.0. 2.1 given.') { LSolr.new(:field).fuzzy_match('word', distance: 2.1).to_s }
+    assert_raise(LSolr::RangeError, 'Out of 0.0..1.0. -0.1 given.') { LSolr.new(:field).fuzzy_match('word', distance: -0.1).to_s }
+    assert_raise(LSolr::RangeError, 'Out of 0.0..1.0. 2.1 given.') { LSolr.new(:field).fuzzy_match('word', distance: 2.1).to_s }
     assert_equal 'field:word~2.0', LSolr.new(:field).fuzzy_match('wo|rd').to_s
   end
 
@@ -127,7 +129,7 @@ class LSolrTest < Test::Unit::TestCase
     assert_equal 'field:[10 TO 20}', LSolr.new(:field).greater_than_or_equal_to(10).less_than(20).to_s
     assert_equal 'field:[10 TO 20]', LSolr.new(:field).greater_than_or_equal_to(10).less_than_or_equal_to(20).to_s
 
-    assert_raise(RuntimeError, 'Please specify a search condition.') { LSolr.new(:field).greater_than(10).to_s }
+    assert_raise(LSolr::IncompleteQueryError, 'Please specify a search condition.') { LSolr.new(:field).greater_than(10).to_s }
 
     from = Date.new(2000, 1, 1)
     to = Date.new(3000, 12, 31)
